a756a67867e220fbb33b46df0eceb8cf48cba31e
cossmikus <kassymkhan.bolat@nu.edu.kz>
2023-11-23 03:07:26 +0600

    dfdgffd

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from datetime import datetime, timezone
 import os
-import psycopg2
 from dotenv import load_dotenv
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
@@ -12,6 +11,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+
 class User(db.Model):
     __tablename__ = 'THE_USER'
     user_id = db.Column(db.Integer, primary_key=True)
@@ -22,6 +22,7 @@ class User(db.Model):
     phone_number = db.Column(db.String)
     profile_description = db.Column(db.String)
     the_password = db.Column(db.String)
+
 
 @app.route("/api/user", methods=["POST"])
 def create_user():
@@ -38,84 +39,62 @@ def create_user():
         the_password=data.get("the_password")
     )
 
-    with app.app_context():
-        db.session.add(user)
-        db.session.commit()
+    db.session.add(user)
+    db.session.commit()
 
     return jsonify({"user_id": user.user_id, "message": "User created successfully"}), 201
 
+
 @app.route("/api/users", methods=["GET"])
 def get_all_users():
-    with connection.cursor() as cursor:
-        cursor.execute("SELECT * FROM THE_USER;")
-        all_users_data = cursor.fetchall()
-        if all_users_data:
-            columns = [desc[0] for desc in cursor.description]
-            users_list = [dict(zip(columns, user)) for user in all_users_data]
-            return jsonify(users_list)
-        else:
-            return jsonify({"message": "No users found"}), 404
+    all_users_data = User.query.all()
+    if all_users_data:
+        users_list = [user.__dict__ for user in all_users_data]
+        return jsonify(users_list)
+    else:
+        return jsonify({"message": "No users found"}), 404
+
 
 @app.route("/api/user/<int:user_id>", methods=["GET"])
 def get_user(user_id):
-    with connection.cursor() as cursor:
-        cursor.execute("SELECT * FROM THE_USER WHERE user_id = %s;", (user_id,))
-        user_data = cursor.fetchone()
-        if user_data:
-            return jsonify(user_data)
-        else:
-            return jsonify({"message": "User not found"}), 404
+    user_data = User.query.get(user_id)
+    if user_data:
+        return jsonify(user_data.__dict__)
+    else:
+        return jsonify({"message": "User not found"}), 404
+
 
 @app.route("/api/user/<int:user_id>", methods=["PATCH"])
 def update_user(user_id):
-    with connection.cursor() as cursor:
-        data = request.get_json()
-        email = data.get("email")
-        given_name = data.get("given_name")
-        surname = data.get("surname")
-        city = data.get("city")
-        phone_number = data.get("phone_number")
-        profile_description = data.get("profile_description")
-        password = data.get("the_password")
+    user = User.query.get(user_id)
+    if not user:
+        return jsonify({"message": "User not found"}), 404
 
-        set_clause = []
-        if email is not None:
-            set_clause.append(f"email = '{email}'")
-        if given_name is not None:
-            set_clause.append(f"given_name = '{given_name}'")
-        if surname is not None:
-            set_clause.append(f"surname = '{surname}'")
-        if city is not None:
-            set_clause.append(f"city = '{city}'")
-        if phone_number is not None:
-            set_clause.append(f"phone_number = '{phone_number}'")
-        if profile_description is not None:
-            set_clause.append(f"profile_description = '{profile_description}'")
-        if password is not None:
-            set_clause.append(f"the_password = '{password}'")
+    data = request.get_json()
+    user.email = data.get("email", user.email)
+    user.given_name = data.get("given_name", user.given_name)
+    user.surname = data.get("surname", user.surname)
+    user.city = data.get("city", user.city)
+    user.phone_number = data.get("phone_number", user.phone_number)
+    user.profile_description = data.get("profile_description", user.profile_description)
+    user.the_password = data.get("the_password", user.the_password)
 
-        if set_clause:
-            update_query = f"""
-                UPDATE THE_USER 
-                SET {', '.join(set_clause)} 
-                WHERE user_id = %s;
-            """
-            cursor.execute(update_query, (user_id,))
-            if cursor.rowcount > 0:
-                return jsonify({"message": f"User with ID {user_id} updated successfully"})
-            else:
-                return jsonify({"message": "User not found"}), 404
-        else:
-            return jsonify({"message": "No valid fields provided for update"}), 400
+    db.session.commit()
+
+    return jsonify({"message": f"User with ID {user_id} updated successfully"})
+
 
 @app.route("/api/user/<int:user_id>", methods=["DELETE"])
 def delete_user(user_id):
-    with connection.cursor() as cursor:
-        cursor.execute("DELETE FROM THE_USER WHERE user_id = %s;", (user_id,))
-        if cursor.rowcount > 0:
-            return jsonify({"message": "User deleted successfully"})
-        else:
-            return jsonify({"message": "User not found"}), 404
+    user = User.query.get(user_id)
+    if not user:
+        return jsonify({"message": "User not found"}), 404
+
+    db.session.delete(user)
+    db.session.commit()
+
+    return jsonify({"message": "User deleted successfully"})
+
 
 if __name__ == "__main__":
     app.run(debug=os.getenv("FLASK_DEBUG", False))
